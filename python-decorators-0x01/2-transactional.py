c7f228cab383260c1c3677603d40cac6fe7d452b
amlfarrag3 <farragaml983@gmail.com>
2025-05-23 19:43:14 +0300

    Create 2-transactional.py

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,12 @@
+import sqlite3
+import functools
+
+# Decorator to automatically manage DB connection
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            result = func(conn, *args, **kwargs)
+        finally:
+            conn.
