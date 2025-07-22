c7f457729c020f529004ff18e1bd25e898619932
amlfarrag3 <farragaml983@gmail.com>
2025-07-22 17:41:16 +0300

    Update serializers.py

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,20 +1,20 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError  # For custom validation
 from .models import User, Conversation, Message
 
 
 # === User Serializer ===
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(read_only=True)
+
     class Meta:
         model = User
-        fields = [
-            'user_id',
-            'first_name',
-            'last_name',
-            'email',
-            'phone_number',
-            'role',
-            'created_at',
-        ]
+        fields = ['user_id', 'first_name', 'last_name', 'email','phone_number','role','created_at',]
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['full_name'] = f"{instance.first_name} {instance.last_name}"
+        return data
 
 
 # === Message Serializer ===
@@ -30,6 +30,8 @@ class MessageSerializer(serializers.ModelSerializer):
             'sent_at',
         ]
 
+    def get_sender_name(self, obj):
+        return f"{obj.sender.first_name} {obj.sender.last_name}"
 
 # === Conversation Serializer (with nested messages + participants) ===
 class ConversationSerializer(serializers.ModelSerializer):
@@ -44,3 +46,11 @@ class ConversationSerializer(serializers.ModelSerializer):
             'messages',
             'created_at',
         ]
+
+    def validate_participant_ids(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("At least two participants are required.")
+        return value
+
+
+
