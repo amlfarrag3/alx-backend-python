e9a8793a3a044f9c1c388726e44ba092b4e8d313
amlfarrag3 <farragaml983@gmail.com>
2025-07-22 17:17:36 +0300

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -124,3 +124,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'chats.User'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # <- Enforces login by default
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',  # for browsable API
+        'rest_framework.authentication.BasicAuthentication',    # for tools like Postman
+    ],
+}
