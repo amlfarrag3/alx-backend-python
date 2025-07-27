4354394611013fb14918fa7c1eba43476f9ea06c
amlfarrag3 <farragaml983@gmail.com>
2025-07-27 09:19:00 +0300

    Update models.py

--- Django-signals_orm-0x04/chats/models.py
+++ Django-signals_orm-0x04/chats/models.py
@@ -80,6 +80,8 @@ class Message(models.Model):
     content = models.TextField()
     edited = models.BooleanField(default=False) 
     sent_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(null=True, blank=True)
+    edited_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='edited_messages')
     receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE,null=True, blank=True)
 
     def __str__(self):
