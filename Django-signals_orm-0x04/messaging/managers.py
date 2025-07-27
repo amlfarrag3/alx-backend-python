f1ba32bc3e2f6df997a18ac572a5db4c9df61e1b
amlfarrag3 <farragaml983@gmail.com>
2025-07-27 11:03:57 +0300

    Update managers.py

--- Django-signals_orm-0x04/messaging/managers.py
+++ Django-signals_orm-0x04/messaging/managers.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 class UnreadMessagesManager(models.Manager):
-    def for_user(self, user):
-        return self.get_queryset().filter(receiver=user, read=False).only('message_body', 'sender', 'sent_at')   
+    def unread_for_user(self, user):
+        return self.get_queryset().filter(receiver=user, read=False).only('message_body', 'sender', 'sent_at')
+
