52f2274eeff25c1d71ef3996ebba1e4daee26de0
amlfarrag3 <farragaml983@gmail.com>
2025-07-27 09:16:36 +0300

    Update signals.py

--- Django-signals_orm-0x04/chats/signals.py
+++ Django-signals_orm-0x04/chats/signals.py
@@ -2,6 +2,23 @@ from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import Message, Notification, MessageHistory  # Include MessageHistory
 from django.core.exceptions import ObjectDoesNotExist
+from django.utils import timezone
+from django.db.models.signals import post_delete
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    # Delete messages sent or received
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+
+    # Delete related notifications
+    Notification.objects.filter(user=instance).delete()
+
+    # Delete message histories of messages sent by this user
+    MessageHistory.objects.filter(message__sender=instance).delete()
 
 
 @receiver(post_save, sender=Message)
@@ -15,17 +32,17 @@ def create_notification_on_message(sender, instance, created, **kwargs):
 
 @receiver(pre_save, sender=Message)
 def log_message_edit(sender, instance, **kwargs):
-    # Only run on updates (not new message creation)
     if instance.pk:
         try:
             old_message = Message.objects.get(pk=instance.pk)
         except ObjectDoesNotExist:
             return
 
-        # Compare message body before and after change
         if old_message.message_body != instance.message_body:
             MessageHistory.objects.create(
                 message=instance,
                 previous_content=old_message.message_body
             )
             instance.edited = True
+            instance.edited_at = timezone.now()
+
